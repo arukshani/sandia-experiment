3f5d735f8aa8641e0f189432837fe022562a7f1d
rukshani <rukshani@localhost>
2022-06-16 16:10:11 -0600

    Cloudlab machine setup

--- scripts/setup/install-dependencies.sh
+++ scripts/setup/install-dependencies.sh
@@ -16,12 +16,13 @@ tar zxf MLNX_OFED_LINUX-5.5-1.0.3.2-ubuntu20.04-x86_64.tgz
 cd MLNX_OFED_LINUX-5.5-1.0.3.2-ubuntu20.04-x86_64
 
 sudo ./mlnxofedinstall --upstream-libs
-sudo /etc/init.d/openibd restart
+#Restart the machine, then restart openid
+# sudo /etc/init.d/openibd restart
 
-# Install Mellanxo IRQ affinity tools if needed. Modern OFED often includes these tools.
-if ! [ -x "`command -v set_irq_affinity_bynode.sh`" ]; then
-    wget http://www.mellanox.com/relateddocs/prod_software/mlnx_irq_affinity.tgz
-    tar xzf mlnx_irq_affinity.tgz --directory=/usr/sbin/ --overwrite
-fi
+# # Install Mellanxo IRQ affinity tools if needed. Modern OFED often includes these tools.
+# if ! [ -x "`command -v set_irq_affinity_bynode.sh`" ]; then
+#     wget http://www.mellanox.com/relateddocs/prod_software/mlnx_irq_affinity.tgz
+#     tar xzf mlnx_irq_affinity.tgz --directory=/usr/sbin/ --overwrite
+# fi
 
 cd "$cwd"
