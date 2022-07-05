3fdfe66a26906a6e03e8719a48a8065e66e4863d
rukshani <rukshani@localhost>
2022-07-05 17:33:25 -0600

    Install memtier benchmark tool

---
+++ scripts/memcached/install-memtier-benchmark.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt-get install build-essential autoconf automake libpcre3-dev libevent-dev pkg-config zlib1g-dev libssl-dev
+
+sudo chown -R $USER /opt
+cd /opt
+echo $(pwd)
+git clone git@github.com:RedisLabs/memtier_benchmark.git
+cd /opt/memtier_benchmark
+echo $(pwd)
+autoreconf -ivf
+./configure
+make
+sudo make install
+
+#memtier_benchmark --help
